221a91bcd7f0fd5fb930391e4f79e907f9d4a2b1
adivaveronia <adiva310802@gmail.com>
2021-09-11 10:57:57 +0700

    commit

--- jmart/adivaJmartFH/Jmart.java
+++ jmart/adivaJmartFH/Jmart.java
@@ -1,35 +1,26 @@
 package adivaJmartFH;
 
-
-/**
- * Write a description of class Jmart here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class Jmart
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Jmart
-     */
-    public Jmart()
-    {
-        // initialise instance variables
-        x = 0;
+    public static void main (String[] args){
+        System.out.println("test");
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public int getPromo () {
+        return 0;
+    }
+    public String getCustomer () {
+        return "oop";
+    }
+    public float getDiscountPercentage(int before, int after){
+        float discount = 0;
+        if (before > after) {
+            discount = before - after;
+        }
+        else if (before <= after) {
+            discount = 0;
+        }
+        return discount;
     }
 }
+    
+    
