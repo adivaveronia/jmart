5e243ead681a4fb5042d6b766259cba2efb1bcac
adivaveronia <adiva310802@gmail.com>
2021-11-05 21:59:23 +0700

    Modul 6 - Adiva Veronia - Tugas Pendahuluan

--- src/adivaJmartFH/Jmart.java
+++ src/adivaJmartFH/Jmart.java
@@ -1,24 +1,37 @@
 package adivaJmartFH;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import com.google.gson.*;
+
 public class Jmart
 {
-    public Jmart() {
-        // belum diisi
+    class Country{
+        public String name;
+        public int population;
+        public List<String> listOfStates;
     }
-    public Product create() {
-        return null;
-    }
-    public static Product createProduct() {
-        return null;
-    }
-    public static Coupon createCoupon() {
-        return null;
-    }
-    
+
     public static void main(String[] args) {
-        
+        String filepath = "C:/Users/adiva/Documents/jmart/city.json";
+        Gson gson = new Gson();
+        try
+        {
+            BufferedReader br = new BufferedReader(new FileReader(filepath));
+            Country input = gson.fromJson(br, Country.class);
+            System.out.println("name: " + input.name);
+            System.out.println("population: " + input.population);
+            System.out.println("states: ");
+            input.listOfStates.forEach(state -> System.out.println(state));
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
- 
+
 }
     
     
